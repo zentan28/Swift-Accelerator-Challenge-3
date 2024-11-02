ca0ba3db5876455199ebc983bce4e2e56993630e
ZenTan <zentan2801@gmail.com>
2024-11-02 11:37:33 +0800

    Initial Commit

---
+++ Swift Accelerator Challenge 3/Swift_Accelerator_Challenge_3App.swift
@@ -0,0 +1,17 @@
+//
+//  Swift_Accelerator_Challenge_3App.swift
+//  Swift Accelerator Challenge 3
+//
+//  Created by T Krobot on 2/11/24.
+//
+
+import SwiftUI
+
+@main
+struct Swift_Accelerator_Challenge_3App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Swift Accelerator Challenge 3Tests/Swift_Accelerator_Challenge_3Tests.swift
@@ -0,0 +1,17 @@
+//
+//  Swift_Accelerator_Challenge_3Tests.swift
+//  Swift Accelerator Challenge 3Tests
+//
+//  Created by T Krobot on 2/11/24.
+//
+
+import Testing
+@testable import Swift_Accelerator_Challenge_3
+
+struct Swift_Accelerator_Challenge_3Tests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
