d68afd8a466d9c0e00257a575d3533dc2f2db2bd
Calvin Abad <calvin.abad@gmail.com>
2024-11-09 16:05:43 +0800

    Local contacts struct reduced to supplementary information for CNContact store.

--- Swift Accelerator Challenge 3/Models and Viewmodels/Contact.swift
+++ Swift Accelerator Challenge 3/Models and Viewmodels/Contact.swift
@@ -5,28 +5,11 @@
 //  Created by Calvin Abad on 4/11/24.
 //
 import Foundation
-struct Contact: Identifiable, Codable{
-    let id = UUID()
-    var identifier: String
-    var name: String
-    var image: Data
-    var birthday: Date
-    var phonenumber: String
+import Contacts
+struct Contact: Codable {
+    var identifier: UUID
     var other: String
     var notes: String
     var reminders: [Reminder]
-    
-    static func == (lhs: Contact, rhs: Contact) -> Bool {
-        return lhs.identifier == rhs.identifier
-    }
-    
-//    static func == (lhs: Contact, rhs: Contact) -> Bool {
-//            return lhs.id == rhs.id &&
-//                   lhs.name == rhs.name &&
-//                   lhs.image == rhs.image &&
-//                   lhs.birthday == rhs.birthday &&
-//                   lhs.phonenumber == rhs.phonenumber
-//        }
     var discardedReminders: [Reminder]
-        //    var contactCategory: ContactCategory
 }
