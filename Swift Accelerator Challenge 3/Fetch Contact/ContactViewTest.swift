ccd7bf4684e167d60180642f47ed689e13f0a5bd
ZenTan <zentan2801@gmail.com>
2024-11-09 09:48:36 +0800

    commit with file

---
+++ Swift Accelerator Challenge 3/Fetch Contact/ContactViewTest.swift
@@ -0,0 +1,7 @@
+//
+//  ContactViewTest.swift
+//  Swift Accelerator Challenge 3
+//
+//  Created by T Krobot on 9/11/24.
+//
+
---
+++ Swift Accelerator Challenge 3/Models and Viewmodels/ContactManager.swift
@@ -0,0 +1,7 @@
+//
+//  ContactManager.swift
+//  Swift Accelerator Challenge 3
+//
+//  Created by T Krobot on 9/11/24.
+//
+
